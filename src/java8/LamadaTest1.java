3488c431bef2b2ef729a78ed260aa80d3054e6b6
Dhanashri11Pusadkar <dhanashripusadkar@gmail.com>
2022-12-20 16:12:53 +0530

    Exception

---
+++ src/java8/LamadaTest1.java
@@ -0,0 +1,37 @@
+package java8;
+@FunctionalInterface
+interface collage
+{
+    void collageDetailes();
+    default void takeaddmission()
+    {
+        System.out.println("i m from addmission methos");
+    }
+}
+class JSPM implements collage
+{
+
+    @Override
+    public void collageDetailes() {
+        System.out.println("this is JSPM collage");
+    }
+}
+class AISSMS implements collage
+{
+    @Override
+    public void collageDetailes() {
+        System.out.println("this is AISSMS collage");
+    }
+}
+
+public class LamadaTest1 {
+    public static void main(String[] args) {
+        collage c1=new JSPM();
+        c1.collageDetailes();
+        c1.takeaddmission();
+
+        collage c2=new AISSMS();
+        c2.collageDetailes();
+        c2.takeaddmission();
+    }
+}
